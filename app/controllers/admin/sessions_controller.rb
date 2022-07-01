bb95aef2810482719857f84f7fd3f3b0c2ce4a6e
yossu-y <yangyuub@gmail.com>
2022-07-01 08:49:49 +0000

    [update]admin-login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,7 +18,15 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_in_path_for(resources)
+    admin_users_path
+  end
+
+  def after_sign_out_path_for(resources)
+    root_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
