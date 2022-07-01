a93b62926f4eeed33c2dc4c26c3f08951a2263c8
yossu-y <yangyuub@gmail.com>
2022-07-01 05:31:21 +0000

    [update]user-show.edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,31 @@
 class Public::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def index
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました！"
+    else
+      render "edit"
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:screen_name, :introduction, :profile_image)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  has_many :posts, dependent: :destroy
+
+
+
+  def get_profile_image
+    (profile_image.attached?)? profile_image: "no-image-icon.jpg"
+  end
+
 end
