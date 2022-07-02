d237dd4597110f412d36fda6ee7d91d502a0eaf1
yossu-y <yangyuub@gmail.com>
2022-07-02 15:43:20 +0000

    [update]order

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,30 @@
+class Public::OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.user_id = current_user.id
+    @order = Order.new(order_params)
+    # @order = current_user.orders.find_by(post_id: params[:order][:post_id])
+    @order.save
+    redirect_to posts_path, notice: "購入が完了しました。ありがとうございました！"
+  end
+
+  def order
+    @post = Post.find(params[:post_id])
+  end
+
+  def thanks
+  end
+
+  def user_orders
+    @orders = current_user.orders.all
+  end
+
+  private
+
+  def order_params
+    params.permit(:post_id, :user_id)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_043740) do
+ActiveRecord::Schema.define(version: 2022_07_02_134011) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 2022_07_02_043740) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
