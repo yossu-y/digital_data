e122e587c794aca250ad48ff4b3f37ac4817937e
yossu-y <yangyuub@gmail.com>
2022-07-02 04:59:15 +0000

    [update]genre

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  belongs_to :genre
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
 
 
