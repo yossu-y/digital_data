06d54265e9c3d8efb735156c2cbb0b64f2df0d40
yossu-y <yangyuub@gmail.com>
2022-07-03 08:26:37 +0000

    [update]searches

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,11 +1,11 @@
 class Public::SearchesController < ApplicationController
 
   def search
-    @keyword = params[:keyword]
+    @range = params[:range]
 
-    if @keyword == "ユーザー"
+    if @range == "ユーザー"
       @users = User.search(params[:search], params[:keyword])
-    else @keyword = "出品"
+    else @range = "出品"
       @posts = Post.search(params[:search], params[:keyword])
     end
 
