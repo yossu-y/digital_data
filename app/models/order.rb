9dfb3f9634e40bcd30583d3cb882cc9c31b0340c
yossu-y <yangyuub@gmail.com>
2022-07-03 06:44:35 +0000

    [update]post.image

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ApplicationRecord
+  has_one_attached :image
 
   belongs_to :user
   belongs_to :post
+
+  def get_image
+    (image.attached?)? image: "no-image-icon.jpg"
+  end
+
 end
