7abc91731fa2838b082ba861720804cc6a67d792
yossu-y <yangyuub@gmail.com>
2022-07-27 11:23:44 +0000

    [update]user-edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -37,7 +37,7 @@ class Public::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:screen_name, :introduction, :profile_image)
+    params.require(:user).permit(:screen_name, :introduction, :email, :profile_image)
   end
 
 end
